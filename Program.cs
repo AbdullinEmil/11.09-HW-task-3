b7e31bf8207e2a4f611f2b934d88654359fca0e8
admin <admin@DESKTOP-H8R9H0U>
2024-10-15 21:31:40 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите строку: ");
+        string inputString = Console.ReadLine();
+
+        Console.Write("Введите сдвиг для шифра Цезаря: ");
+        if (int.TryParse(Console.ReadLine(), out int shift))
+        {
+            string encryptedString = EncryptCaesar(inputString, shift);
+            Console.WriteLine($"Зашифрованная строка: {encryptedString}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод сдвига.");
+        }
+    }
+
+    static string EncryptCaesar(string text, int shift)
+    {
+        char[] chars = text.ToCharArray();
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (char.IsLetter(chars[i]))
+            {
+                bool isUpper = char.IsUpper(chars[i]);
+                char baseChar = isUpper ? 'A' : 'a';
+                chars[i] = (char)((chars[i] - baseChar + shift) % 26 + baseChar);
+            }
+        }
+        return new string(chars);
+    }
+}
